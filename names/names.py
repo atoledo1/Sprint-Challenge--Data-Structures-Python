7b496eda621e5f96b588c360b17a5e75b0c73378
adriana <atoledosantiago1@gmail.com>
2020-08-14 12:51:00 -0500

    mvp

--- names/names.py
+++ names/names.py
@@ -3,24 +3,66 @@ import time
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
+names_1 = f.read().split("\n")  
 f.close()
 
 f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
+names_2 = f.read().split("\n")  
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+duplicates = [] 
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = Node(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = Node(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
+
+
+
+bTree = Node(names_1[0])
+for name in names_1:
+    bTree.insert(name)
+
+
+for name in names_2:
+    if bTree.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
+
+
+
+#runtime: 0.12067890167236328 seconds
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
